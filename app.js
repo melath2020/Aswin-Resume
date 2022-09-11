e38059eff272d79f6553f50897881371ec2edcac
Aswinvaasits <84697530+Aswinvaasits@users.noreply.github.com>
2022-09-11 19:13:01 +0530

    update

--- app.js
+++ app.js
@@ -68,6 +68,15 @@ function scrollFunction() {
   }
 }
 
+  // tabs
+  function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+  }
 
 
 // mobile nav
